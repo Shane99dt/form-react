b4992a677d6c884ce043cc50a0f353180d311ff2
Shane99dt <dushen1999thilaksha@gmail.com>
2022-07-18 17:25:52 +0200

    basically works

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
-const listtable = []
+// const listtable = []
 const regex = /^[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/
+let borderMail = "2px solid red"
+let borderpassword = "2px solid red"
 
 class App extends React.Component {
   constructor(){
@@ -19,12 +21,16 @@ class App extends React.Component {
 
 handleSubmit = (e) => {
   e.preventDefault()
-  // if(this.state.emailIsValid && this.state.passwordIsValid){
+  if(this.state.emailIsValid && this.state.passwordIsValid){
+    this.setState({
+      isSubmitted : true
+    })
+    console.log(this.state)
+    this.resetValues()
 
-  // }
-  console.log(this.state)
-  console.log(listtable)
-  this.resetState()
+  }else{
+    alert('Fill the fields correctly')
+  }
 }
 
 // handleChange = (e) => {
@@ -44,7 +50,7 @@ handleEmailChange = (e) => {
     this.setState({
       emailIsValid: true,
     })
-
+    borderMail = "2px solid green"
   }
 }
 
@@ -55,10 +61,11 @@ handlePasswordChange =(e) => {
 
   // un petit chose a faire avec le length, >=5 ou >5
   if(this.state.password.length > 5){
-    console.log(this.state.password)
     this.setState({
       passwordIsValid: true
     })
+    borderpassword = "2px solid green"
+
   }
 }
 
@@ -69,7 +76,7 @@ handleRememberMeChange =(e) => {
 }
 
 
-resetState = () => {
+resetValues = () => {
   this.setState({
     email: "",
     password: "",
@@ -78,27 +85,39 @@ resetState = () => {
     passwordIsValid: false,
     isSubmitted: false
   })
+  borderMail = "2px solid red"
+  borderpassword = "2px solid red"
 }
 
   render() {
     return (
       <div className="d-flex align-items-center flex-column mt-4">
         <h1>Login</h1>
-        <form onSubmit={this.handleSubmit}>
-          <div className="mb-3">
-            <label htmlFor="emailInput" className="form-label">Email address</label>
-            <input type="email" name='email' className="form-control" id="emailInput" aria-describedby="emailHelp" value={this.state.email} onChange={this.handleEmailChange} required/>
-          </div>
-          <div className="mb-3">
-            <label htmlFor="passwordInput" className="form-label">Password</label>
-            <input type="password" name='password' className="form-control" id="exampleInputPassword1" value={this.state.password} onChange={this.handlePasswordChange} required/>
-          </div>
-          <div className="mb-3 form-check">
-            <input type="checkbox" name='rememberMe' className="form-check-input" id="exampleCheck1" value={this.state.rememberMe} onChange={this.handleRememberMeChange}/>
-            <label className="form-check-label" htmlFor="exampleCheck1">Remember me</label>
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
+        {this.state.isSubmitted ? <p>Hello here</p> :
+          <form onSubmit={this.handleSubmit}>
+
+            {/* email */}
+            <div className="mb-3">
+              <label htmlFor="emailInput" className="form-label">Email address</label>
+              <input type="email" name='email' className="form-control" id="emailInput" aria-describedby="emailHelp" value={this.state.email} onChange={this.handleEmailChange} required style={{border: borderMail}}/>
+            </div>
+
+            {/* password */}
+            <div className="mb-3">
+              <label htmlFor="passwordInput" className="form-label">Password</label>
+              <input type="password" name='password' className="form-control" id="exampleInputPassword1" value={this.state.password} onChange={this.handlePasswordChange} required style={{border: borderpassword}}/>
+            </div>
+
+            {/* checkbox */}
+            <div className="mb-3 form-check">
+              <input type="checkbox" name='rememberMe' className="form-check-input" id="exampleCheck1" value={this.state.rememberMe} onChange={this.handleRememberMeChange}/>
+              <label className="form-check-label" htmlFor="exampleCheck1">Remember me</label>
+            </div>
+
+            {/* submit */}
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        }
       </div>
     );
   }
